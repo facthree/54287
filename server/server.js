c3d2afd3cd8315fb23754af5faa3f52e5900135e
facthree <86576196+facthree@users.noreply.github.com>
2023-04-18 19:04:49 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,33 +21,19 @@ app.get('/', async (req, res) => {
   })
 })
 
-async function createCompletionWithRetry(prompt, retries = 3, delay = 500) {
-  try {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0,
-      max_tokens: 3000,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0,
-    });
-    return response;
-  } catch (error) {
-    if (retries > 0 && error.status === 429) {
-      await new Promise((resolve) => setTimeout(resolve, delay));
-      return createCompletionWithRetry(prompt, retries - 1, delay * 2);
-    } else {
-      throw error;
-    }
-  }
-}
-
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await createCompletionWithRetry(prompt);
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    });
 
     res.status(200).send({
       bot: response.data.choices[0].text
